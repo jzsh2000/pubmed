64b6f312f8e080d7bfa697527d05be7d8c5f943e
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-07-22 02:28:17 +0800

    Add various pubmed record formats
    using pmid 20000000

---
+++ data/pubmed/example/fetch-data.sh
@@ -0,0 +1,7 @@
+efetch -db pubmed -id 20000000 -format abstract > 20000000.abstract
+efetch -db pubmed -id 20000000 -format asn.1 > 20000000.asn.1
+efetch -db pubmed -id 20000000 -format docsum > 20000000.docsum
+efetch -db pubmed -id 20000000 -format docsum -mode json > 20000000.json
+efetch -db pubmed -id 20000000 -format medline > 20000000.medline
+efetch -db pubmed -id 20000000 -format url > 20000000.url
+efetch -db pubmed -id 20000000 -format xml > 20000000.xml
