ec546a54675463cea0a14c59f6c5e313b0b529f4
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-07-22 00:56:24 +0800

    Fix script path

--- script/run-extract.sh
+++ script/run-extract.sh
@@ -2,14 +2,15 @@
 
 input_dir='data/pubmed/ftp.ncbi.nlm.nih.gov/pubmed/baseline'
 output_dir='data/pubmed/clean_data'
+script_dir=$(dirname $0)
 mkdir -p $output_dir/{publication-date,mesh-term,publication-type,content,date}
 find $input_dir -name '*.gz' \
-    | parallel -j1 --verbose "./extract-pubdate.sh {} > $output_dir/publication-date/{/.}.pubdate"
+    | parallel -j1 --verbose "$script_dir/extract-pubdate.sh {} > $output_dir/publication-date/{/.}.pubdate"
 find $input_dir -name '*.gz' \
-    | parallel -j1 --verbose "./extract-mesh.sh {} > $output_dir/mesh-term/{/.}.mesh"
+    | parallel -j1 --verbose "$script_dir/extract-mesh.sh {} > $output_dir/mesh-term/{/.}.mesh"
 find $input_dir -name '*.gz' \
-    | parallel -j1 --verbose "./extract-publication-type.sh {} > $output_dir/publication-type/{/.}.pt"
+    | parallel -j1 --verbose "$script_dir/extract-publication-type.sh {} > $output_dir/publication-type/{/.}.pt"
 find $input_dir -name '*.gz' \
-    | parallel -j1 --verbose "./extract-title-abstract.sh {} > $output_dir/content/{/.}.content"
+    | parallel -j1 --verbose "$script_dir/extract-title-abstract.sh {} > $output_dir/content/{/.}.content"
 find $input_dir -name '*.gz' \
-    | parallel -j1 --verbose "./extract-date.sh {} > $output_dir/date/{/.}.date"
+    | parallel -j1 --verbose "$script_dir/extract-date.sh {} > $output_dir/date/{/.}.date"
