31dee931e56ef81d7b18c87fa72fbd362fef0f3d
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-07-24 23:50:40 +0800

    Solve 'is not sorted' problem

--- script/count-mesh.sh
+++ script/count-mesh.sh
@@ -2,19 +2,21 @@
 
 cat data/pubmed/clean_data/mesh-term/*.mesh \
     | cut -f2 \
+    | grep '^D' \
     | sort \
     | uniq -c \
     | awk 'BEGIN{OFS="\t"}{print $2,$1}' \
-    | sort -k1 \
+    | sort -k1b,1 \
     > data/pubmed/clean_data/mesh.all.txt
 
 cat data/pubmed/clean_data/mesh-term/*.mesh \
     | awk '$3=="Y"' \
     | cut -f2 \
+    | grep '^D' \
     | sort \
     | uniq -c \
     | awk 'BEGIN{OFS="\t"}{print $2,$1}' \
-    | sort -k1 \
+    | sort -k1b,1 \
     > data/pubmed/clean_data/mesh.major.txt
 
 join -a 1 -e 0 -o auto data/pubmed/clean_data/{mesh.all.txt,mesh.major.txt} \
