51849d0abdeb06d9ee047e44b6688ceda41bdfe3
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-08-05 23:56:29 +0800

    Get first and last author of pubmed articles

---
+++ script/get-first-last-author.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -ueo pipefail
+
+cat data/pubmed/clean_data/author/*.author \
+    | awk -F'\t' \
+       'BEGIN {OFS="\t"; i=0; line=""} \
+       {
+           if($1 != i) {
+               if(line != "") {
+                   print line
+                   line=""
+               }
+               print
+               i=$1
+           }
+           else {line = $0}
+       }'
