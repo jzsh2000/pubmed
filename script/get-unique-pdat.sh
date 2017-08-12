42f01ed84a358a96a8b925cb0d4d1419a5555874
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-08-13 00:15:55 +0800

    Sort by pmid first

--- script/get-unique-pdat.sh
+++ script/get-unique-pdat.sh
@@ -3,6 +3,7 @@
 cat data/pubmed/clean_data/publication-date/*.pubdate \
     | awk '$2=="pubmed"' \
     | cut -f1,3 \
+    | sort -k1b,1 \
     | awk 'BEGIN{i=0}{if($1!=i){i=$1;print}}' \
     | sort -k1b,1 \
     > data/pubmed/clean_data/pubmed-pdat.txt
