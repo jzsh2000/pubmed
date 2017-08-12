b37ffe988475be1ffe76a56bc2c17d3aaf6adcba
Xiaoyang Jin <jinxiaoyang@jinxiaoyang.cn>
2017-08-12 23:57:08 +0800

    Fix syntax error in awk

--- script/get-unique-pdat.sh
+++ script/get-unique-pdat.sh
@@ -3,6 +3,6 @@
 cat data/pubmed/clean_data/publication-date/*.pubdate \
     | awk '$2=="pubmed"' \
     | cut -f1,3 \
-    | awk 'BEGIN{i=0}if($1!=i){i=$1;print}'
+    | awk 'BEGIN{i=0}{if($1!=i){i=$1;print}}'
     | sort -k1b,1 \
     > data/pubmed/clean_data/pubmed-pdat.txt
